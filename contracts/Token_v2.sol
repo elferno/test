7c539e01fb1af11d94a9baef533b06f6a58f58e9
elferno <elferno@inbox.ru>
2021-05-28 09:46:30 +0500

    v.1.0.1

--- contracts/Token_v2.sol
+++ contracts/Token_v2.sol
@@ -6,6 +6,12 @@ import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
 contract Token_v2 is ERC20Upgradeable {
     uint256 public constant testConstant = 2;
 
+    /*
+    function initialize(string memory _name, string memory _symbol) public initializer {
+        __ERC20_init(_name, _symbol);
+    }
+    */
+
     function buyToken() public pure returns (string memory) {
         return 'v.2 - "buyToken" function works!';
     }
